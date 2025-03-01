ef843691453567b9f52c02956f6dc8ddbbe0b3cc
Vandana-jain-123 <vandana12345jain@gmail.com>
2025-03-01 17:05:00 +0530

    added component navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/component/Navbar'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
     <h1>Hello appp data or base </h1>
+    <Navbar/>
      </>
   )
 }
--- src/components/component/Navbar.jsx
+++ src/components/component/Navbar.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { Link } from "react-router-dom"
+
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <nav className='navbar'>
+        <Link to="/">Home</Link>
+        <Link to="/myBook"></Link>
+        <Link to="/loginUser">Login/Register</Link>
+        <Link to="/myBook"></Link>
+        <Link to="/loginUser">Login/Register</Link>
+
+      </nav>
+
+
+    </>
   )
 }
 
